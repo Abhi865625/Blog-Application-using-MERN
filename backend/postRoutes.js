56d5c4f729067fb0b6c96b644d6a6e8df2ef9db0
Abhi865625 <akmehta7252@gmail.com>
2024-07-31 13:45:06 +0530

    new file:   backend/.gitignore 	new file:   backend/connect.js 	new file:   backend/package-lock.json 	new file:   backend/package.json 	new file:   backend/postRoutes.js 	new file:   backend/server.js 	new file:   frontend/.eslintrc.cjs 	new file:   frontend/.gitignore 	new file:   frontend/README.md 	new file:   frontend/index.html 	new file:   frontend/package-lock.json 	new file:   frontend/package.json 	new file:   frontend/public/vite.svg 	new file:   frontend/src/App.css 	new file:   frontend/src/App.jsx 	new file:   frontend/src/assets/react.svg 	new file:   frontend/src/index.css 	new file:   frontend/src/main.jsx 	new file:   frontend/vite.config.js

---
+++ backend/postRoutes.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const database = require("./connect");
+const ObjectId = require("mongodb").ObjectId;
+
+let postRoutes = express.Router();
+
+// 1 - Retrieve All
+postRoutes.route("/posts").get(async (request, response) => {
+    try {
+        let db = database.getDb();
+        let data = await db.collection("posts").find({}).toArray();
+
+        if (data.length > 0) {
+            response.json(data);
+        } else {
+            response.status(404).json({ message: "No data found." });
+        }
+    } catch (error) {
+        response.status(500).json({ message: error.message });
+    }
+});
+
+// 2 - Retrieve One
+postRoutes.route("/posts/:id").get(async (request, response) => {
+    try {
+        let db = database.getDb();
+        let data = await db.collection("posts").findOne({ _id: new ObjectId(request.params.id) });
+
+        if (data) {
+            response.json(data);
+        } else {
+            response.status(404).json({ message: "Data not found." });
+        }
+    } catch (error) {
+        response.status(500).json({ message: error.message });
+    }
+});
+
+// 3 - Create One
+postRoutes.route("/posts").post(async (request, response) => {
+    try {
+        let db = database.getDb();
+        let mongoObject = {
+            title: request.body.title,
+            description: request.body.description,
+            content: request.body.content,
+            author: request.body.author,
+            dateCreated: request.body.dateCreated
+        };
+        let data = await db.collection("posts").insertOne(mongoObject);
+        response.status(201).json(data);
+    } catch (error) {
+        response.status(500).json({ message: error.message });
+    }
+});
+
+// 4 - Update One
+postRoutes.route("/posts/:id").put(async (request, response) => {
+    try {
+        let db = database.getDb();
+        let mongoObject = {
+            $set: {
+                title: request.body.title,
+                description: request.body.description,
+                content: request.body.content,
+                author: request.body.author,
+                dateCreated: request.body.dateCreated
+            }
+        };
+        let data = await db.collection("posts").updateOne({ _id: new ObjectId(request.params.id) }, mongoObject);
+        response.json(data);
+    } catch (error) {
+        response.status(500).json({ message: error.message });
+    }
+});
+
+// 5 - Delete One
+postRoutes.route("/posts/:id").delete(async (request, response) => {
+    try {
+        let db = database.getDb();
+        let data = await db.collection("posts").deleteOne({ _id: new ObjectId(request.params.id) });
+
+        if (data.deletedCount > 0) {
+            response.json({ message: "Post deleted." });
+        } else {
+            response.status(404).json({ message: "Post not found." });
+        }
+    } catch (error) {
+        response.status(500).json({ message: error.message });
+    }
+});
+
+module.exports = postRoutes;
